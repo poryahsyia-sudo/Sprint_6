fe5a74fe9eec852e1732cfc8e5c3865e86b4af7a
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-26 13:57:22 +0500

    .

--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class OrderPageLocators:
-    # --- Первый шаг ---
+    # Первый шаг
     NAME_FIELD = (By.XPATH, "//input[@placeholder='* Имя']")
     SURNAME_FIELD = (By.XPATH, "//input[@placeholder='* Фамилия']")
     ADDRESS_FIELD = (By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']")
@@ -10,7 +10,7 @@ class OrderPageLocators:
     PHONE_FIELD = (By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']")
     NEXT_BUTTON = (By.XPATH, "//button[text()='Далее']")
 
-    # --- Второй шаг ---
+    # Второй шаг 
     DATE_FIELD = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']")
     RENTAL_PERIOD_DROPDOWN = (By.XPATH, "//div[@class='Dropdown-placeholder']")
     RENTAL_PERIOD_OPTION_TEMPLATE = "//div[@class='Dropdown-option' and text()='{}']"
@@ -19,3 +19,8 @@ class OrderPageLocators:
     ORDER_BUTTON = (By.XPATH, "//button[text()='Заказать']")
     CONFIRM_BUTTON = (By.XPATH, "//button[text()='Да']")
     ORDER_SUCCESS_POPUP = (By.XPATH, "//div[contains(text(), 'Заказ оформлен')]")
+
+    # Кнокпи
+    ORDER_BUTTON_TOP = (By.XPATH, "(//button[text()='Заказать'])[1]")
+    ORDER_BUTTON_BOTTOM = (By.XPATH, "(//button[text()='Заказать'])[2]")
+
