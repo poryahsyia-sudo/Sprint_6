6aaa7243850fd4f1116c74017a56a585b660c1c9
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-26 13:40:05 +0500

    /

--- pages/order_page.py
+++ pages/order_page.py
@@ -11,26 +11,6 @@ class OrderPage:
         self.driver = driver
         self.wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
 
-    # --- ДОБАВЛЕНО ---
-    def open_main_page(self):
-        """Открывает главную страницу"""
-        self.driver.get("https://qa-scooter.praktikum-services.ru/")
-
-    def click_top_order_button(self):
-        """Клик по верхней кнопке 'Заказать'"""
-        button = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "(//button[text()='Заказать'])[1]"))
-        )
-        button.click()
-
-    def click_bottom_order_button(self):
-        """Клик по нижней кнопке 'Заказать'"""
-        button = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "(//button[text()='Заказать'])[2]"))
-        )
-        button.click()
-    # --- КОНЕЦ ДОБАВЛЕННОГО ---
-
     # Заполнение поля "Имя"
     def fill_name(self, name):
         field = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.NAME_FIELD))
@@ -49,18 +29,26 @@ class OrderPage:
         field.clear()
         field.send_keys(address)
 
-    # Выбор станции метро
+   
+#  Выбор станции метро из выпадающего списка
     def select_metro_station(self, metro_name):
+    # Находим и кликаем по полю метро
         metro_input = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.METRO_FIELD))
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", metro_input)
         metro_input.click()
 
+        # Прокручиваем до нужной станции
         metro_option = self.wait.until(
-            EC.presence_of_element_located(OrderPageLocators.METRO_OPTION)
+            EC.presence_of_element_located((By.XPATH, OrderPageLocators.METRO_OPTION_TEMPLATE.format(metro_name)))
         )
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", metro_option)
-        metro_option = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.METRO_OPTION))
+
+        # Явно ждём кликабельности и кликаем
+        metro_option = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, OrderPageLocators.METRO_OPTION_TEMPLATE.format(metro_name)))
+        )
         metro_option.click()
+
 
     # Заполнение поля "Телефон"
     def fill_phone(self, phone):
@@ -73,7 +61,6 @@ class OrderPage:
         button = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.NEXT_BUTTON))
         button.click()
 
-    # Заполнение даты и периода
     def fill_date_and_select_rental_period(self, date_text, period_text):
         date_field = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.DATE_FIELD))
         date_field.click()
@@ -89,7 +76,7 @@ class OrderPage:
             EC.element_to_be_clickable((By.XPATH, f"//div[@class='Dropdown-option' and text()='{period_text}']"))
         )
         option.click()
-
+    
     # Выбор цвета самоката
     def select_scooter_color(self):
         color = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.COLOR_BLACK))
@@ -105,12 +92,12 @@ class OrderPage:
         button = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.ORDER_BUTTON))
         button.click()
 
-    # Подтверждение во всплывающем окне
+    # Подтверждение в всплывающем окне
     def confirm_order(self):
         confirm = self.wait.until(EC.element_to_be_clickable(OrderPageLocators.CONFIRM_BUTTON))
         confirm.click()
 
-    # Проверка успешного окна
+    #  Проверка окна успешного оформления заказа
     def check_success_popup(self):
         popup = self.wait.until(EC.visibility_of_element_located(OrderPageLocators.SUCCESS_POPUP))
         return popup.is_displayed()
--- tests/test_order_flow.py
+++ tests/test_order_flow.py
@@ -13,53 +13,26 @@ from pages.base_page import BasePage
 class TestOrderFlow:
 
     @pytest.mark.parametrize("user", ORDER_USERS)
-    @allure.title("Оформление заказа через верхнюю кнопку 'Заказать'")
-    def test_order_from_top_button(self, driver, user):
-        main = BasePage(driver)
-        main.open_page()
-        main.click_top_order_button()
-
+    @allure.title("Оформление заказа через  кнопку 'Заказать'")
+    def test_order_scooter_positive(self, driver, order_button, test_data): 
+        base_page = BasePage(driver)
         order_page = OrderPage(driver)
-
-        # --- Первый шаг ---
-        order_page.fill_name(user["first_name"])
-        order_page.fill_surname(user["last_name"])
-        order_page.fill_address(user["address"])
-        order_page.select_metro_station(user["metro_station"])
-        order_page.fill_phone(user["phone"])
+        if order_button == "top":
+            base_page.click_order_top()
+        else:
+            base_page.click_order_bottom()
+        order_page.fill_name(test_data["name"])
+        order_page.fill_surname(test_data["surname"])
+        order_page.fill_address(test_data["address"])
+        order_page.select_metro_station(test_data["metro"])
+        order_page.fill_phone(test_data["phone"])
         order_page.click_next_button()
-
-        # --- Второй шаг ---
-        order_page.fill_date_and_select_rental_period("30.10.2025", "двое суток")
-        order_page.select_scooter_color()
-        order_page.fill_comment(user["comment"])
+        order_page.fill_date_and_select_rental_period(test_data["date"], "сутки")
+        order_page.fill_comment(test_data["comment"])
         order_page.click_order_button()
         order_page.confirm_order()
-
-        assert order_page.check_success_popup(), "Сообщение об успешном заказе не появилось"
-
-    @pytest.mark.parametrize("user", ORDER_USERS)
-    @allure.title("Оформление заказа через нижнюю кнопку 'Заказать'")
-    def test_order_from_bottom_button(self, driver, user):
-        main = BasePage(driver)
-        main.open_page()
-        main.click_bottom_order_button()
-
-        order_page = OrderPage(driver)
-
-        # --- Первый шаг ---
-        order_page.fill_name(user["first_name"])
-        order_page.fill_surname(user["last_name"])
-        order_page.fill_address(user["address"])
-        order_page.select_metro_station(user["metro_station"])
-        order_page.fill_phone(user["phone"])
-        order_page.click_next_button()
-
-        # --- Второй шаг ---
-        order_page.fill_date_and_select_rental_period("31.10.2025", "один день")
-        order_page.select_scooter_color()
-        order_page.fill_comment(user["comment"])
-        order_page.click_order_button()
-        order_page.confirm_order()
-
-        assert order_page.check_success_popup(), "Сообщение об успешном заказе не появилось"
+        WebDriverWait(driver, DEFAULT_TIMEOUT).until(
+            EC.visibility_of_element_located(OrderPageLocators.ORDER_SUCCESS_POPUP)
+        )
+        success_text = driver.find_element(*OrderPageLocators.ORDER_SUCCESS_POPUP).text
+        assert "Заказ оформлен" in success_text, "Окно успешного заказа не появилось"
