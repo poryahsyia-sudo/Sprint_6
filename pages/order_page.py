6ab043728ada700183d567e0e5677eafb03ceb9a
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-26 12:28:30 +0500

    .

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,64 +1,68 @@
-import allure
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from pages.base_page import BasePage
-from locators.main_page_locators import MainPageLocators
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
 from locators.order_page_locators import OrderPageLocators
+from pages.base_page import BasePage
+from config import DEFAULT_TIMEOUT
 
 
 class OrderPage(BasePage):
 
-    @allure.step("Клик по верхней кнопке 'Заказать'")
-    def click_top_order_button(self):
-        button = self.scroll_to_element(MainPageLocators.TOP_ORDER_BUTTON)
-        self.wait.until(EC.element_to_be_clickable(MainPageLocators.TOP_ORDER_BUTTON))
-        button.click()
+    def fill_first_step(self, first_name, last_name, address, metro, phone):
+        """Заполнение первого шага формы заказа"""
+        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
 
-    @allure.step("Клик по нижней кнопке 'Заказать'")
-    def click_bottom_order_button(self):
-        button = self.scroll_to_element(MainPageLocators.BOTTOM_ORDER_BUTTON)
-        self.wait.until(EC.element_to_be_clickable(MainPageLocators.BOTTOM_ORDER_BUTTON))
-        button.click()
+        wait.until(EC.presence_of_element_located(OrderPageLocators.FIRST_NAME_FIELD)).send_keys(first_name)
+        self.driver.find_element(*OrderPageLocators.LAST_NAME_FIELD).send_keys(last_name)
+        self.driver.find_element(*OrderPageLocators.ADDRESS_FIELD).send_keys(address)
 
-    @allure.step("Заполнение первой части формы заказа: {first_name} {last_name}")
-    def fill_first_step(self, first_name: str, last_name: str, address: str, metro_station: str, phone: str):
-        self.wait_and_send_keys(OrderPageLocators.NAME_FIELD, first_name)
-        self.wait_and_send_keys(OrderPageLocators.SURNAME_FIELD, last_name)
-        self.wait_and_send_keys(OrderPageLocators.ADDRESS_FIELD, address)
-        self.wait_and_click(OrderPageLocators.METRO_FIELD)
-        metro_option = (By.XPATH, OrderPageLocators.METRO_OPTION_TEMPLATE.format(metro_station))
-        self.scroll_to_element(metro_option)
-        self.wait_and_click(metro_option)
-        self.wait_and_send_keys(OrderPageLocators.PHONE_FIELD, phone)
-        self.wait_and_click(OrderPageLocators.NEXT_BUTTON)
+        # Выбор станции метро
+        metro_field = self.driver.find_element(*OrderPageLocators.METRO_FIELD)
+        metro_field.click()
+        wait.until(EC.presence_of_all_elements_located(OrderPageLocators.METRO_OPTION))[0].click()
 
-    @allure.step("Заполнение второй части формы заказа: дата={date}, срок={rental_period}")
-    def fill_second_step(self, date: str, rental_period: str, scooter_color: str, comment: str):
-        self.wait_and_click(OrderPageLocators.DATE_FIELD)
-        self.wait_and_send_keys(OrderPageLocators.DATE_FIELD, date)
-        self.driver.find_element(*OrderPageLocators.DATE_FIELD).send_keys(Keys.ENTER)
-        self.wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "react-datepicker")))
-        self.wait_and_click(OrderPageLocators.RENTAL_PERIOD_DROPDOWN)
-        rental_option = (By.XPATH, OrderPageLocators.RENTAL_PERIOD_OPTION_TEMPLATE.format(rental_period))
-        self.scroll_to_element(rental_option)
-        self.wait_and_click(rental_option)
-        color = scooter_color.lower()
-        if color == "black":
-            self.wait_and_click(OrderPageLocators.COLOR_BLACK)
-        elif color in ("grey", "gray"):
-            self.wait_and_click(OrderPageLocators.COLOR_GREY)
-        self.wait_and_send_keys(OrderPageLocators.COMMENT_FIELD, comment)
-        self.wait_and_click(OrderPageLocators.ORDER_BUTTON)
+        self.driver.find_element(*OrderPageLocators.PHONE_FIELD).send_keys(phone)
+        self.driver.find_element(*OrderPageLocators.NEXT_BUTTON).click()
 
-    @allure.step("Подтверждение заказа")
+    def fill_second_step(self, date, period, color, comment):
+        """Заполнение второго шага формы заказа"""
+        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
+
+        # Ввод даты
+        date_field = wait.until(EC.element_to_be_clickable(OrderPageLocators.DATE_FIELD))
+        date_field.click()
+
+        # Дождаться календаря и выбрать число
+        wait.until(EC.visibility_of_element_located(OrderPageLocators.DATEPICKER))
+        date_cell = self.driver.find_element(By.XPATH, f"//div[contains(@class, 'react-datepicker__day') and text()='{date}']")
+        date_cell.click()
+
+        # Выбор периода аренды
+        self.driver.find_element(*OrderPageLocators.RENTAL_PERIOD_FIELD).click()
+        wait.until(EC.element_to_be_clickable(OrderPageLocators.RENTAL_PERIOD_OPTION(period))).click()
+
+        # Выбор цвета
+        self.driver.find_element(*OrderPageLocators.COLOR_CHECKBOX(color)).click()
+
+        # Комментарий
+        self.driver.find_element(*OrderPageLocators.COMMENT_FIELD).send_keys(comment)
+
+        # Клик по кнопке "Заказать"
+        self.driver.find_element(*OrderPageLocators.ORDER_BUTTON).click()
+
     def confirm_order(self):
-        self.wait_and_click(OrderPageLocators.CONFIRM_BUTTON)
+        """Подтверждение заказа"""
+        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
+        wait.until(EC.element_to_be_clickable(OrderPageLocators.YES_BUTTON)).click()
 
-    @allure.step("Проверка успешного оформления заказа")
-    def check_success_message(self) -> bool:
+    def is_order_confirmed(self):
+        """Проверка, что заказ успешно оформлен"""
+        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
         try:
-            self.wait.until(EC.visibility_of_element_located(OrderPageLocators.ORDER_SUCCESS_POPUP))
+            wait.until(EC.visibility_of_element_located(OrderPageLocators.STATUS_MODAL))
             return True
-        except:
+        except TimeoutException:
             return False
