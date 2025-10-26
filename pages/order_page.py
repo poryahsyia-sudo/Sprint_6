7e2c48205a539af70bea1fd483eabb80688c07b4
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-26 12:30:59 +0500

    .

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,68 +1,72 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
 
-from locators.order_page_locators import OrderPageLocators
 from pages.base_page import BasePage
-from config import DEFAULT_TIMEOUT
+from locators.main_page_locators import MainPageLocators
+from locators.order_page_locators import OrderPageLocators
 
 
 class OrderPage(BasePage):
 
-    def fill_first_step(self, first_name, last_name, address, metro, phone):
-        """Заполнение первого шага формы заказа"""
-        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
+    @allure.step("Клик по верхней кнопке 'Заказать'")
+    def click_top_order_button(self):
+        button = self.scroll_to_element(MainPageLocators.TOP_ORDER_BUTTON)
+        self.wait.until(EC.element_to_be_clickable(MainPageLocators.TOP_ORDER_BUTTON))
+        button.click()
 
-        wait.until(EC.presence_of_element_located(OrderPageLocators.FIRST_NAME_FIELD)).send_keys(first_name)
-        self.driver.find_element(*OrderPageLocators.LAST_NAME_FIELD).send_keys(last_name)
-        self.driver.find_element(*OrderPageLocators.ADDRESS_FIELD).send_keys(address)
+    @allure.step("Клик по нижней кнопке 'Заказать'")
+    def click_bottom_order_button(self):
+        button = self.scroll_to_element(MainPageLocators.BOTTOM_ORDER_BUTTON)
+        self.wait.until(EC.element_to_be_clickable(MainPageLocators.BOTTOM_ORDER_BUTTON))
+        button.click()
 
-        # Выбор станции метро
-        metro_field = self.driver.find_element(*OrderPageLocators.METRO_FIELD)
-        metro_field.click()
-        wait.until(EC.presence_of_all_elements_located(OrderPageLocators.METRO_OPTION))[0].click()
+    @allure.step("Заполнение первой части формы заказа: {first_name} {last_name}")
+    def fill_first_step(self, first_name: str, last_name: str, address: str, metro_station: str, phone: str):
+        self.wait_and_send_keys(OrderPageLocators.FIRST_NAME_FIELD, first_name)
+        self.wait_and_send_keys(OrderPageLocators.LAST_NAME_FIELD, last_name)
+        self.wait_and_send_keys(OrderPageLocators.ADDRESS_FIELD, address)
 
-        self.driver.find_element(*OrderPageLocators.PHONE_FIELD).send_keys(phone)
-        self.driver.find_element(*OrderPageLocators.NEXT_BUTTON).click()
+        self.wait_and_click(OrderPageLocators.METRO_FIELD)
+        self.wait.until(EC.presence_of_all_elements_located(OrderPageLocators.METRO_OPTION))[0].click()
 
-    def fill_second_step(self, date, period, color, comment):
-        """Заполнение второго шага формы заказа"""
-        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
+        self.wait_and_send_keys(OrderPageLocators.PHONE_FIELD, phone)
+        self.wait_and_click(OrderPageLocators.NEXT_BUTTON)
 
-        # Ввод даты
-        date_field = wait.until(EC.element_to_be_clickable(OrderPageLocators.DATE_FIELD))
-        date_field.click()
+    @allure.step("Заполнение второй части формы заказа: дата={date}, срок={rental_period}")
+    def fill_second_step(self, date: str, rental_period: str, scooter_color: str, comment: str):
+        # Выбор даты
+        self.wait_and_click(OrderPageLocators.DATE_FIELD)
+        self.wait.until(EC.visibility_of_element_located(OrderPageLocators.DATEPICKER))
 
-        # Дождаться календаря и выбрать число
-        wait.until(EC.visibility_of_element_located(OrderPageLocators.DATEPICKER))
-        date_cell = self.driver.find_element(By.XPATH, f"//div[contains(@class, 'react-datepicker__day') and text()='{date}']")
-        date_cell.click()
+        # Клик по нужному числу, чтобы закрыть календарь
+        date_cell = (By.XPATH, f"//div[contains(@class, 'react-datepicker__day') and text()='{date}']")
+        self.wait_and_click(date_cell)
 
-        # Выбор периода аренды
-        self.driver.find_element(*OrderPageLocators.RENTAL_PERIOD_FIELD).click()
-        wait.until(EC.element_to_be_clickable(OrderPageLocators.RENTAL_PERIOD_OPTION(period))).click()
+        # Выбор срока аренды
+        self.wait_and_click(OrderPageLocators.RENTAL_PERIOD_FIELD)
+        rental_option = OrderPageLocators.RENTAL_PERIOD_OPTION(rental_period)
+        self.wait_and_click(rental_option)
 
         # Выбор цвета
-        self.driver.find_element(*OrderPageLocators.COLOR_CHECKBOX(color)).click()
+        color_checkbox = OrderPageLocators.COLOR_CHECKBOX(scooter_color)
+        self.wait_and_click(color_checkbox)
 
         # Комментарий
-        self.driver.find_element(*OrderPageLocators.COMMENT_FIELD).send_keys(comment)
+        self.wait_and_send_keys(OrderPageLocators.COMMENT_FIELD, comment)
 
-        # Клик по кнопке "Заказать"
-        self.driver.find_element(*OrderPageLocators.ORDER_BUTTON).click()
+        # Кнопка "Заказать"
+        self.wait_and_click(OrderPageLocators.ORDER_BUTTON)
 
+    @allure.step("Подтверждение заказа")
     def confirm_order(self):
-        """Подтверждение заказа"""
-        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
-        wait.until(EC.element_to_be_clickable(OrderPageLocators.YES_BUTTON)).click()
+        self.wait_and_click(OrderPageLocators.YES_BUTTON)
 
-    def is_order_confirmed(self):
-        """Проверка, что заказ успешно оформлен"""
-        wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
+    @allure.step("Проверка успешного оформления заказа")
+    def check_success_message(self) -> bool:
         try:
-            wait.until(EC.visibility_of_element_located(OrderPageLocators.STATUS_MODAL))
+            self.wait.until(EC.visibility_of_element_located(OrderPageLocators.STATUS_MODAL))
             return True
         except TimeoutException:
             return False
