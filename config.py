5b11fe52a9b3978786a9787786f47a9709bc6c97
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-10-26 11:17:11 +0500

    исправлены замеяания ревьюера к тесту logo

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BASE_URL = "https://qa-scooter.praktikum-services.ru/"
-DEFAULT_TIMEOUT = 10  # 10 секунд ожидания для всех явных ожиданий
+YANDEX_URL = "https://dzen.ru/?yredirect=true"
+DEFAULT_TIMEOUT = 10 # 10 секунд ожидания для всех явных ожиданий
---
+++ tests/test_logo_redirects.py
@@ -0,0 +1,25 @@
+import pytest
+import allure
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.main_page import MainPage
+from config import BASE_URL, YANDEX_URL
+
+@allure.feature("Переходы по логотипам")
+class TestClickLogo:
+
+    @allure.title("Проверка перехода по логотипу 'Самокат'")
+    def test_click_scooter_logo_redirects_to_main(self, driver):
+        main = MainPage(driver)
+        main.click_order_top()
+        main.click_scooter_logo()
+        assert main.check_current_url(BASE_URL), "Переход по логотипу 'Самокат' не открыл главную страницу"
+
+    @allure.title("Проверка открытия Дзена по логотипу 'Яндекс'")
+    def test_click_yandex_logo_opens_dzen(self, driver):
+        main = MainPage(driver)
+        main.click_yandex_logo()
+        WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)
+        driver.switch_to.window(driver.window_handles[-1])
+        WebDriverWait(driver, 15).until(EC.url_contains(YANDEX_URL))
+        assert YANDEX_URL in driver.current_url, f"Ожидался переход на {YANDEX_URL}"
